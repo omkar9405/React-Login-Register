3b4386f203632bfa5ed470de63fd13a46f5c20f6
omkar9405 <omkarp1101@rediffmail.com>
2020-07-31 18:36:25 +0530

    Updates

---
+++ precache-manifest.c75703dec08db2aa22554b86a049bb0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b34ccbc89c0be7259762ec749eb8cb",
+    "url": "/React-Login-Register/index.html"
+  },
+  {
+    "revision": "1254c781eb38812f9025",
+    "url": "/React-Login-Register/static/css/main.690c6d06.chunk.css"
+  },
+  {
+    "revision": "ae8c6c2ca9ece762f92e",
+    "url": "/React-Login-Register/static/js/2.2eb26f44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Login-Register/static/js/2.2eb26f44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1254c781eb38812f9025",
+    "url": "/React-Login-Register/static/js/main.35178a9a.chunk.js"
+  },
+  {
+    "revision": "2f1ccf3b9af3103d07f0",
+    "url": "/React-Login-Register/static/js/runtime-main.c5a537aa.js"
+  },
+  {
+    "revision": "cba154e7488165fb665ce412ef7748f5",
+    "url": "/React-Login-Register/static/media/login.cba154e7.svg"
+  }
+]);
